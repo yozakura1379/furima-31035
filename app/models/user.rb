a194f2d32e65e2dd7b6ed0fb1f5ed783cdd95cb8
yozakura1379 <72366000+yozakura1379@users.noreply.github.com>
2020-11-02 16:58:00 +0900

    ユーザー管理機能2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,16 @@ class User < ApplicationRecord
 
   with_options presence: true do # ここでdoにすることで１つ１つやなくて済む
     validates :nickname
-    EMAIL_REGEX = /.+@.+/.freeze # .と+で＠の前は１つ以上の文字列があるという意味になる、.+の間に＠を挟むことで＠を含めないとダメと言うことになる
-    validates_format_of :email, with: EMAIL_REGEX, message: '@を含めて設定してください'
+    # EMAIL_REGEX = /.+@.+/.freeze # .と+で＠の前は１つ以上の文字列があるという意味になる、.+の間に＠を挟むことで＠を含めないとダメと言うことになる
+    # validates_format_of :email, with: EMAIL_REGEX, message: '@を含めて設定してください'
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i.freeze
+    NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/ 
+    NAME_REGEX_ALIAS = /\A[ァ-ヶー－]+\z/ # カタカナのみの場合はンで終わるのではなくケで終わるようにする
     validates_format_of :password, with: PASSWORD_REGEX, message: '6文字以上の半角英数字'
-    validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :firstname_alias, format: { with: /\A[ァ-ヶー－]+\z/ } # カタカナのみの場合はンで終わるのではなくケで終わるようにする
-    validates :lastname_alias, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :firstname, format: { with: NAME_REGEX }
+    validates :lastname, format: { with: NAME_REGEX }
+    validates :firstname_alias, format: { with: NAME_REGEX_ALIAS }
+    validates :lastname_alias, format: { with:  NAME_REGEX_ALIAS }
     validates :birthday
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,11 +6,21 @@ RSpec.describe User, type: :model do
       @user = FactoryBot.build(:user)
     end
 
-    it "firstname,lastname,firstname_alias,lastname_alias,nickname,
+    describe 'ユーザー新規登録' do
+      context '新規登録がうまくいくとき' do
+        it "firstname,lastname,firstname_alias,lastname_alias,nickname,
         birthday,email,password,password_confirmationが存在すれば登録できること" do
       expect(@user).to be_valid
-    end
+     end
+     it 'passwordが６文字以上の半角英数字であれば登録できること' do
+      @user.password = 'aiueo10'
+      @user.password_confirmation = 'aiueo10'
+      expect(@user).to be_valid
+     end
+  end
 
+  
+  context '新規登録がうまくいかないとき' do
     it 'firstnameが空では登録できないこと' do
       @user.firstname = nil
       @user.valid?
@@ -35,6 +45,31 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Lastname alias can't be blank")
     end
 
+
+      it 'firstnameが全角（漢字・ひらがな・カタカナ）以外だとユーザー登録できない' do
+        @user.firstname= 'kamiya'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Firstname is invalid")
+      end
+
+      it 'lastnameが全角（漢字・ひらがな・カタカナ）以外だとユーザー登録できない' do
+        @user.lastname = 'ryota'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Lastname is invalid")
+      end
+
+      it 'firstname_aliasが全角（カタカナ）以外だとユーザー登録できない' do
+        @user.firstname_alias = '神谷'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Firstname alias is invalid")
+      end
+
+      it 'lastname_aliasが全角（カタカナ）以外だとユーザー登録できない' do
+        @user.lastname_alias = '涼太'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Lastname alias is invalid")
+      end
+    
     it 'nicknameが空では登録できないこと' do
       @user.nickname = nil
       @user.valid?
@@ -59,11 +94,7 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password can't be blank")
     end
 
-    it 'passwordが６文字以上の半角英数字であれば登録できること' do
-      @user.password = 'aiueo10'
-      @user.password_confirmation = 'aiueo10'
-      expect(@user).to be_valid
-    end
+    
 
     it 'passwordが5文字以下であれば登録できないこと' do
       @user.password = 'aiu10'
@@ -71,6 +102,21 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)', 'Password 6文字以上の半角英数字')
     end
+
+    it 'passwordが半角英字のみだとユーザー登録できない' do
+      @user.password = 'aiueoa'
+      @user.password_confirmation = 'aiueoa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password 6文字以上の半角英数字")
+    end
+
+    it 'passwordが半角数字のみだとユーザー登録できない' do
+      @user.password = '123456'
+      @user.password_confirmation = '123456'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password 6文字以上の半角英数字")
+    end
+
 
     it 'passwordとpassword_confirmationが不一致では登録できないこと' do
       @user.password = 'aiueo10'
@@ -85,5 +131,18 @@ RSpec.describe User, type: :model do
       another_user.valid?
       expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
+   
+    it 'emailに@が含まれていないとユーザー登録できない' do
+      @user.email = 'kkkgmail.com'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Email is invalid')
+     end
+
+
+
+
+
+    end
+   end
   end
 end
