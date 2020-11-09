eaf65b3bc08ca2bac38386925fe28e6bc96d84f5
yozakura1379 <72366000+yozakura1379@users.noreply.github.com>
2020-11-09 15:05:59 +0900

    商品削除機能1
    1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,7 +30,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-      
+    if @item.destroy
+      redirect_to root_path
+   else
+    render :show
+   end
+
   end
 
   def update
